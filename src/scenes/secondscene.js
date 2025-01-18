cc94a9782b2096967ac4f89c40ecf9ebbfe98d57
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2025-01-18 16:27:48 +0100

    import model

--- src/scenes/secondscene.js
+++ src/scenes/secondscene.js
@@ -1,17 +1,20 @@
+import '@babylonjs/loaders';
+
 async function secondscene(BABYLON, engine, currentScene) {
     const { Vector3, Scene, MeshBuilder, FreeCamera, HemisphericLight } = BABYLON;
 
     const scene = new Scene(engine);
 
     // Créer une boîte pour référence
-    const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 1.5 }, scene);
+    //const sphere = MeshBuilder.CreateSphere("sphere", { diameter: 1.5 }, scene);
+    BABYLON.SceneLoader.ImportMesh("", "https://raw.githubusercontent.com/BabylonJS/MeshesLibrary/master/", "PBR_Spheres.glb", scene);
 
     // Ajouter une lumière hémisphérique
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
     // Créer une caméra ArcRotate
     const canvas = document.getElementById("renderCanvas");
-    const cam = new BABYLON.ArcRotateCamera("arcCamera", BABYLON.Tools.ToRadians(0), BABYLON.Tools.ToRadians(45), 10, sphere.position, scene);
+    const cam = new BABYLON.ArcRotateCamera("arcCamera", BABYLON.Tools.ToRadians(0), BABYLON.Tools.ToRadians(45), 10, new Vector3(0,0,0), scene);
     cam.attachControl(canvas, true);
 
     // Ajuster les limites (optionnel)
